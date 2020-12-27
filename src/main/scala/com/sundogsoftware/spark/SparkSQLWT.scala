1d469a1a89e5c703494788f3956f17921ee78465
hlaingwintunn <hlaingwintunn@gmail.com>
2020-12-27 17:20:52 +0800

    SparkSQL Examples

--- src/main/scala/com/sundogsoftware/spark/SparkSQLWT.scala
+++ src/main/scala/com/sundogsoftware/spark/SparkSQLWT.scala
@@ -1,50 +1,43 @@
 package com.sundogsoftware.spark
 
-import org.apache.spark.sql._
 import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
 
-object SparkSQL {
-  
-  case class Person(ID:Int, name:String, age:Int, numFriends:Int)
-  
+object SparkSQLWT {
+  case class Person(Id:Int, name: String, age: Int, numFriends:Int)
+
   def mapper(line:String): Person = {
-    val fields = line.split(',')  
-    
+    val fields = line.split(',')
+
     val person:Person = Person(fields(0).toInt, fields(1), fields(2).toInt, fields(3).toInt)
     person
   }
-  
-  /** Our main function where the action happens */
-  def main(args: Array[String]) {
-    
-    // Set the log level to only print errors
+
+  def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    
-    // Use new SparkSession interface in Spark 2.0
+
     val spark = SparkSession
       .builder
-      .appName("SparkSQL")
+      .appName("SparkSQLWT")
       .master("local[*]")
       .getOrCreate()
-    
+
     val lines = spark.sparkContext.textFile("data/fakefriends-noheader.csv")
     val people = lines.map(mapper)
-    
-    // Infer the schema, and register the DataSet as a table.
+
     import spark.implicits._
     val schemaPeople = people.toDS
-    
+
     schemaPeople.printSchema()
-    
     schemaPeople.createOrReplaceTempView("people")
-    
-    // SQL can be run over DataFrames that have been registered as a table
+
     val teenagers = spark.sql("SELECT * FROM people WHERE age >= 13 AND age <= 19")
-    
+
     val results = teenagers.collect()
-    
+
     results.foreach(println)
-    
+
     spark.stop()
+
   }
 }
